703ef5f6cc90ef594755f6613fb248e1c084468c
CGeraldRojas <cri.roj1985@gmail.com>
2025-01-29 19:48:46 +0100

    Lagde QuizItem klassen

---
+++ app/src/main/java/com/hvl/no/dat153_obllig1_quizzy/GalleryItem.java
@@ -0,0 +1,19 @@
+package com.hvl.no.dat153_obllig1_quizzy;
+
+public class GalleryItem {
+    private String name;
+    private String imagePath;
+
+    public GalleryItem(String name, String imagePath) {
+        this.name = name;
+        this.imagePath = imagePath;
+    }
+
+    public java.lang.String getName() {
+        return name;
+    }
+
+    public java.lang.String getImagePath() {
+        return imagePath;
+    }
+}
