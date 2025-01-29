fcede735f54bca1b2c0e500432d611e4999614d8
CGeraldRojas <cri.roj1985@gmail.com>
2025-01-29 19:20:37 +0100

    Added pictures

--- app/src/main/java/com/hvl/no/dat153_obllig1_quizzy/GalleryActivity.java
+++ app/src/main/java/com/hvl/no/dat153_obllig1_quizzy/GalleryActivity.java
@@ -8,13 +8,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.hvl.no.dat153_obllig1_quizzy.databinding.ActivityGalleryBinding;
+
 public class GalleryActivity extends AppCompatActivity {
+    private ActivityGalleryBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_gallery);
+        binding = ActivityGalleryBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnGalleryBack.setOnClickListener(v -> finish());
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/hvl/no/dat153_obllig1_quizzy/MainActivity.java
+++ app/src/main/java/com/hvl/no/dat153_obllig1_quizzy/MainActivity.java
@@ -11,23 +11,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.hvl.no.dat153_obllig1_quizzy.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        Button galleryButton = findViewById(R.id.btnGallery);
-
-        galleryButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, GalleryActivity.class);
-                startActivity(intent);
-            }
+        binding.btnGallery.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, GalleryActivity.class);
+            startActivity(intent);
         });
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
